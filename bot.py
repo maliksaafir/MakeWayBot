c2dfad6a65610a7dbe47fef764ecabfc103b3daf
Malik Saafir <msaafir99@gmail.com>
2021-01-27 22:32:18 -0500

    base discord connection

---
+++ bot.py
@@ -0,0 +1,33 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to this guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    response = message.content + "\n\n that's what you sound like"
+    await message.channel.send(response)
+
+
+client.run(TOKEN)
